a78b964c887eaf5d44dd210e73514548df2e814f
Vinicius Crispim <vinicrispim02@hotmail.com>
2023-11-21 20:56:10 -0300

    Slider interval

--- src/pages/Home/Slider/index.tsx
+++ src/pages/Home/Slider/index.tsx
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from 'react';
 import './style';
 import { MdNavigateNext } from "react-icons/md";
 import { MdNavigateBefore } from "react-icons/md";
-import { Slide, SlideImg, SliderContainer, ButtonSlide } from './style';
+import * as S from './style';
+// import { Slide, SlideImg, SliderContainer, ButtonSlide } from './style';
 
 interface SliderProps {
     images: string[];
@@ -12,16 +13,16 @@ interface SliderProps {
 const Slider: React.FC<SliderProps> = ({ images, interval = 3000 }) => {
     const [activeIndex, setActiveIndex] = useState(0);
 
-    // useEffect(() => {
-    //     const timer = setInterval(() => {
-    //         if (activeIndex === images.length - 1) {
-    //             setActiveIndex(0);
-    //         } else {
-    //             setActiveIndex((prevIndex) => (prevIndex + 1));
-    //         }
-    //     }, interval);
-    //     return () => clearInterval(timer);
-    // }, [activeIndex, images, interval]);
+    useEffect(() => {
+        const timer = setInterval(() => {
+            if (activeIndex === images.length - 1) {
+                setActiveIndex(0);
+            } else {
+                setActiveIndex((prevIndex) => (prevIndex + 1));
+            }
+        }, interval);
+        return () => clearInterval(timer);
+    }, [activeIndex, images, interval]);
     const handlePrevClick = () => {
         setActiveIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
     };
@@ -30,22 +31,22 @@ const Slider: React.FC<SliderProps> = ({ images, interval = 3000 }) => {
         setActiveIndex((prevIndex) => (prevIndex + 1) % images.length);
     };
     return (
-        <SliderContainer>
+        <S.SliderContainer>
             {images.map((image, index) => (
-                <Slide
+                <S.Slide
                     key={image}
                     className={`carousel-slide ${index === activeIndex ? 'active' : ''}`}
                 >
-                    <SlideImg src={image} alt="carousel slide" />
-                </Slide>
+                    <S.SlideImg src={image} alt="carousel slide" />
+                </S.Slide>
             ))}
-            <ButtonSlide className="carousel-button prev" onClick={handlePrevClick}>
+            <S.ButtonSlide className="carousel-button prev" onClick={handlePrevClick}>
                 <MdNavigateBefore />
-            </ButtonSlide>
-            <ButtonSlide className="carousel-button next" onClick={handleNextClick}>
+            </S.ButtonSlide>
+            <S.ButtonSlide className="carousel-button next" onClick={handleNextClick}>
                 <MdNavigateNext />
-            </ButtonSlide>
-        </SliderContainer>
+            </S.ButtonSlide>
+        </S.SliderContainer>
     );
 };
 
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,16 +2,20 @@ import Slider from "./Slider";
 import Banner1 from '../../assets/promo-banner-1.png';
 import Banner2 from '../../assets/promo-banner-2.png';
 import Banner3 from '../../assets/promo-banner-3.png';
+import Table from "./Table";
 
-export const Home = () =>{
- const images = [
-  Banner1,
-  Banner2,
-  Banner3
- ];
-    return(
+export const Home = () => {
+  const images = [
+    Banner1,
+    Banner2,
+    Banner3
+  ];
+  return (
+    <>
       <Slider images={images} interval={5000} />
-    )
-  }
+      <Table />
+    </>
+  )
+}
 
-  export default Home;
+export default Home;
